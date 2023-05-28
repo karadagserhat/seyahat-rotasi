1069ffdeaec8dac9dea3828e293a433adc9e02a7
karadagserhat <serhatkaradagg@outlook.com>
2023-05-28 10:10:45 +0300

    Changed deployment to render.com

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
-export const API_URL = 'https://city-api-production.up.railway.app/api/v1/tours/';
+export const API_URL = 'https://city-api-80b3.onrender.com/api/v1/tours/';
 export const TIMEOUT_SEC = 2;
 export const RES_PER_PAGE = 10;
